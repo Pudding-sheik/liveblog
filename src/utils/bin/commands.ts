69b4b7991e9dfeb52be1c3825aebe943a105ec4a
布丁 <2523896119@qq.com>
2022-06-14 08:34:27 +0800

    fix bug 2

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -153,7 +153,7 @@ export const banner = (args?: string[]): string => {
                                                                                    ░░██████  
                                                                                     ░░░░░░   
 
-Type 'help' to see the list of available commands.whitespace-pre-wrap mb-2
+Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
