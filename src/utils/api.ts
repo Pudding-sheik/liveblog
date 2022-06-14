577d7207f8f82b17b9eb9179cfc1d736030a0941
布丁 <2523896119@qq.com>
2022-06-14 08:37:59 +0800

    fix bug 3

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import config from '../../config.json';
 
-export const getProjects = async () => {
-  const { data } = await axios.get(
-    `https://api.github.com/users/${config.social.github}/repos`,
-  );
-  return data;
-};
+// export const getProjects = async () => {
+//   const { data } = await axios.get(
+//     `https://api.github.com/users/${config.social.github}/repos`,
+//   );
+//   return data;
+// };
 
 export const getReadme = async () => {
   const { data } = await axios.get(config.readmeUrl);
